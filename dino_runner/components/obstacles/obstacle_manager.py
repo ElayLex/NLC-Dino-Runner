976c7fd4cf4785862407d2338e31c49df6a4ef33
ElayLex <duranchavezeliasandre@gmail.com>
2022-10-06 17:43:22 -0400

    clase 2: tarea

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,6 +2,7 @@ import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS
 
 
 class ObstacleManger:
@@ -11,8 +12,10 @@ class ObstacleManger:
     def update(self, game):
         if len(self.obstacles) == 0:
             small_cactus = Cactus(SMALL_CACTUS)
+            large_cactus = Cactus(LARGE_CACTUS)
             self.obstacles.append(small_cactus)
-
+            self.obstacles.append(large_cactus)
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
